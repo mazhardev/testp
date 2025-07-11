70eac974ceefa681f1b4a64124e90ecf67d20ff5
Chaudhry <mazharchdev@gmail.com>
2025-07-11 21:56:08 +0500

    adad

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-baseURL: 'http://localhost:3000', 
+// baseURL: 'http://localhost:3000', 
+baseURL: 'ec2-44-212-51-157.compute-1.amazonaws.com:3000', 
 });
 
 api.interceptors.response.use(
